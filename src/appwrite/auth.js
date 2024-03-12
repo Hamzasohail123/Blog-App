492c2893d81a813a22360d85acc58de60036a979
Hamzasohail123 <hamzasohail429@gmail.com>
2024-03-12 15:13:30 +0500

    appwrite auth service using classes, createAccount, loginAccount, getCurrentAccount, logOutUser done

---
+++ src/appwrite/auth.js
@@ -0,0 +1,67 @@
+import {Client, Account, ID} from 'appwrite';
+import conf from '../config';
+
+export class AuthService{
+    Client = new Client();
+    account;
+
+    constructor(){
+        this.Client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.projectId);
+        this.account = new Account(this.Client);
+    }
+
+    // Create Account
+    async createAccount({email,password,name}){
+        try {
+            const userAccount = this.account.create(ID.unique(),email,password,name);
+            if (userAccount) {
+                // call another method
+                return this.loginAccount({email, password});
+                
+            } else {
+                return userAccount;                
+            }
+            
+        } catch (error) {
+            return error
+        }
+    }
+
+    // Login Account
+    async loginAccount({email,password}){
+        try {
+            return await this.account.createEmailSession(email, password);            
+        } catch (error) {
+            throw error            
+        }
+    }
+
+    // Get Current User
+    async getCurrentUser(){
+        try {
+            return await this.account.get();   
+            
+        } catch (error) {
+            throw error            
+        }
+        return null
+    }
+
+    // Logout Account
+    async logoutAccount(){
+        try {
+            return await this.account.deleteSessions();            
+        } catch (error) {
+            throw error
+            
+        }
+    }
+
+    
+}
+
+const authService = new AuthService();
+
+export default authService;
---
+++ src/conf/conf.js
@@ -0,0 +1,14 @@
+const conf = {
+    appwriteUrl: 
+    String(import.meta.env.REACT_APP_APPWRITE_URL),
+    projectId: 
+    String(import.meta.env.REACT_APP_PROJECT_ID),
+    collectionId:
+     String(import.meta.env.REACT_APP_COLLECTION_ID),
+    databaseId: 
+    String(import.meta.env.REACT_APP_DATABASE_ID),
+    storageBucket:
+    String(import.meta.env.REACT_APP_STORAGE_BUCKET),
+}
+
+export default conf
