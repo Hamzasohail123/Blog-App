0e499bd47cf590ce07a857b9b9cf6fba71bff9b8
Hamzasohail123 <hamzasohail429@gmail.com>
2024-03-15 15:03:01 +0500

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
-    <h1>Blog app with appwrite</h1>
+    <h1>Blog App using Appwrite Backend and Database</h1>
     </>
   )
 }
