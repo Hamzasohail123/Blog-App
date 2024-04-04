4a5c3a2bcfc0dc25dfca1807f94b3d335208a94a
Hamzasohail123 <hamzasohail429@gmail.com>
2024-04-04 17:21:53 +0500

    some changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,44 +1,44 @@
 import React from 'react';
-import {Container, Logo, LoutoutBtn} from '../index'
+import { Container, Logo, LogoutBtn } from '../index';
 import { Link } from 'react-router-dom';
-import {useSelector} from 'react/redux';
-import {useNavigate} from 'react-router-dom'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  const authStatus = useSelector((state)=>{state.auth.status});
-  const nevigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
 
   const navItems = [
     {
       name: 'Home',
-      slug: "/",
+      slug: '/',
       active: true
-    }, 
+    },
     {
-      name: "Login",
-      slug: "/login",
-      active: !authStatus,
-  },
-  {
-      name: "Signup",
-      slug: "/signup",
-      active: !authStatus,
-  },
-  {
-      name: "All Posts",
-      slug: "/all-posts",
-      active: authStatus,
-  },
-  {
-      name: "Add Post",
-      slug: "/add-post",
-      active: authStatus,
-  },
-  ]
+      name: 'Login',
+      slug: '/login',
+      active: !authStatus
+    },
+    {
+      name: 'Signup',
+      slug: '/signup',
+      active: !authStatus
+    },
+    {
+      name: 'All Posts',
+      slug: '/all-posts',
+      active: authStatus
+    },
+    {
+      name: 'Add Post',
+      slug: '/add-post',
+      active: authStatus
+    }
+  ];
 
   return (
     <div>header</div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
