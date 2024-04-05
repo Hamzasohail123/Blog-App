5e8185f8452a7eb6fce80431c8417f5bc950b59f
Hamzasohail123 <hamzasohail429@gmail.com>
2024-04-05 17:16:12 +0500

    button input components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -37,7 +37,35 @@ const Header = () => {
   ];
 
   return (
-    <div>header</div>
+    <div>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link>
+            <Logo width='70px'/>
+            </Link>
+          </div>
+          <ul className='felx ml-auto'>
+            {
+              navItems.map((item)=>{
+                item.active ? (
+                  <li key={item.name}>
+                    <button onClick={() => navigate(item.slug)}
+                    className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                    >{item.name}</button>
+                  </li>
+                ) : null
+              })             
+            }
+             {
+                authStatus && (
+                  <li><LogoutBtn /> </li>
+                )
+              }
+          </ul>
+        </nav>
+      </Container>
+    </div>
   );
 };
 
