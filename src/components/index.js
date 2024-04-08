33b2f4d2a54a3b929829b0f4f8cac0ad2122a706
Hamzasohail123 <hamzasohail429@gmail.com>
2024-04-08 17:08:47 +0500

    login component

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,17 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from '../components/Logo';
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button';
+import Input from './Input';
+import Select from "./Select";
+
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    Button,
+    Select
 }
